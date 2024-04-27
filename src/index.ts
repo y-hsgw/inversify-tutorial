a2d3b5567b3678b390143dbe35dc5c341324c508
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-04-27 18:46:25 +0900

    resolveメソッドの挙動確認

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { container } from "./inversify.config";
 import { Ninja } from "./ninja";
 
-const samurai = container.get(Ninja);
+const samurai = container.resolve(Ninja);
 
 console.log(samurai.fight()); // cut!
 samurai.sneak();
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,9 +1,7 @@
 import { Container } from "inversify";
-import { Ninja } from "./ninja";
 import { Katana } from "./Katana";
 import { Shuriken } from "./shuriken";
 
 export const container = new Container();
-container.bind<Ninja>(Ninja).toSelf();
 container.bind<Katana>(Katana).toConstantValue(new Katana("cut!"));
 container.bind<Shuriken>(Shuriken).toSelf();
